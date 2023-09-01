b60bff78bf26d892154f58474557b6eecdba41b1
Ivylata Das <102812606+Ivylata@users.noreply.github.com>
2023-09-01 20:43:14 +0000

    container config done

---
+++ 01-Basic/variable.js
@@ -0,0 +1,21 @@
+const accountId = 110999
+let accountEmail = "ivy@gmail.com"
+var accountPassword = "52526"
+accountCity ="Kolkata"
+let accountState
+
+// accountId = 2; // as accountId is a const it is not allowed
+
+accountEmail = "ivylata@gmail.com"
+accountPassword = "78786"
+accountCity="Delhi"
+
+console.log(accountId);
+
+console.table([accountEmail,accountPassword,accountCity,accountState])
+
+/*
+There are mainly two ways to declare variable in Javascript
+one is using let another is var
+but var is not prefered because of functional scopes and block scopes means {} 
+*/
